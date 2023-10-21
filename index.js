1e43512e8cf47c2621070cbf095f76c6a7a15cd3
Yuvraj404s <118869535+Yuvraj404s@users.noreply.github.com>
2023-10-21 20:03:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ document.querySelectorAll("button")[0].addEventListener("click",function calling
 
 var randomNumber1=Math.ceil(Math.random()*6);
 var randomDice="dice"+randomNumber1+".png";
-var randomImageSource="./images/"+ randomDice;
+var randomImageSource="./"+ randomDice;
 var image1=document.querySelectorAll("img")[0];
 image1.setAttribute("src",randomImageSource);
 var randomNumber2=Math.ceil(Math.random()*6);
-var randomImageSource2="./images/dice"+ randomNumber2+".png";
+var randomImageSource2="./dice"+ randomNumber2+".png";
 document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
 if(randomNumber1>randomNumber2){
     document.querySelector("h1").innerHTML="🚩 Player 1 is the winner";
